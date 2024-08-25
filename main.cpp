b13ca6f103807e1e842c9b08a884ee0952f18ca2
ivan.qin <qmm161@163.com>
2024-08-25 10:59:17 +0800

    print gamer on map

--- main.cpp
+++ main.cpp
@@ -1,10 +1,16 @@
 #include <iostream>
 #include <ctime>
 #include <cstdlib>
-#include <queue>
-#include <pthread.h>
+#include "ts_queue.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <unistd.h>
+#include "../../../../Library/Developer/CommandLineTools/SDKs/MacOSX13.3.sdk/usr/include/sys/termios.h"
 
 using namespace std;
+using namespace gdface;
 
 // 👦👧
 // 🏤🌋🗻🏠🏡🏢🏣🏤🏥🏦🏨🏩🏪🏫🏬🏭🏯🏰💒🗼🗽⛪⛲🎠🎡🎢🎪⛽🚧🗿
@@ -54,6 +60,11 @@ typedef struct _Block
         return strlen(str);
     }
 
+    bool isBlank()
+    {
+        return str == BLANK;
+    }
+
     const char *str;
 
 private:
@@ -64,19 +75,28 @@ char _Block::BLANK[3] = {' ', ' ', '\0'};
 
 typedef enum
 {
+    OPER_INVALID,
     OPER_LEFT,
     OPER_RIGHT,
     OPER_UP,
     OPER_DOWN
 } OPER;
 
-static queue<OPER> opers;
+threadsafe_queue<OPER> operQueue;
 
-int main()
+static void
+reset_cursor()
 {
-    srand(time(NULL)); // use current time as seed for random generator
-    init_pics();
+    printf("%c[%d;%dH", 27, 0, 0);
+}
 
+static void clear_screen()
+{
+    printf("\e[1;1H\e[2J");
+}
+
+static void *thread_func(void *arg)
+{
     Block units[10][10] = {
         {0, 1, 1, 1, 1, 0, 1, 0, 0, 0},
         {0, 1, 0, 1, 0, 0, 1, 0, 1, 1},
@@ -89,15 +109,124 @@ int main()
         {0, 0, 0, 0, 1, 0, 0, 0, 1, 0},
         {1, 0, 0, 0, 1, 0, 0, 1, 1, 1}};
 
-    for (int line = 0; line < 10; line++)
+    int pos_c = 4;
+    int pos_l = 1;
+
+    while (true)
     {
-        for (auto u : units[line])
+        clear_screen();
+        reset_cursor();
+
+        OPER op = OPER_INVALID;
+        bool keyin = operQueue.try_pop(op);
+        // cout << "receive:" << op << endl;
+        if (keyin)
         {
-            cout << u.str;
+            switch (op)
+            {
+            case OPER_DOWN:
+                pos_l = ((pos_l + 1) < 10 && units[pos_l + 1][pos_c].isBlank()) ? pos_l + 1 : pos_l;
+                break;
+            case OPER_UP:
+                pos_l = ((pos_l - 1) >= 0 && units[pos_l - 1][pos_c].isBlank()) ? pos_l - 1 : pos_l;
+                break;
+            case OPER_LEFT:
+                pos_c = ((pos_c - 1) >= 0 && units[pos_l][pos_c - 1].isBlank()) ? pos_c - 1 : pos_c;
+                break;
+            case OPER_RIGHT:
+                pos_c = ((pos_c + 1) < 10 && units[pos_l][pos_c + 1].isBlank()) ? pos_c + 1 : pos_c;
+                break;
+            }
+        }
+
+        for (size_t line = 0; line < 10; line++)
+        {
+            for (size_t column = 0; column < 10; column++)
+            {
+                if (line == pos_l && column == pos_c)
+                {
+                    cout << "👧";
+                }
+                else
+                {
+
+                    cout << units[line][column].str;
+                }
+            }
+            cout << endl;
         }
         cout << endl;
+        usleep(20000);
     }
-    cout << endl;
+}
 
+static OPER get_user_op(int keyin)
+{
+    OPER op;
+    switch (keyin)
+    {
+    case 'w':
+        op = OPER_UP;
+        break;
+    case 'a':
+        op = OPER_LEFT;
+        break;
+    case 's':
+        op = OPER_DOWN;
+        break;
+    case 'd':
+        op = OPER_RIGHT;
+        break;
+    default:
+        op = OPER_INVALID;
+        break;
+    }
+    return op;
+}
+
+char nonblocking_input(void)
+{
+    char input_ch;
+    struct termios newt, oldt;
+
+    int tty = open("/dev/tty", O_RDONLY); // Open control terminal
+
+    tcgetattr(tty, &oldt); // Get terminal properties
+    newt = oldt;
+
+    // Set characters are not buffered(~ICANON) and do not echo(~ECHO).
+    // You can also choose only one of them.
+    newt.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(tty, TCSANOW, &newt);
+
+    read(tty, &input_ch, 1);
+    tcsetattr(tty, TCSANOW, &oldt); // Restore terminal properties
+
+    return input_ch;
+}
+
+int main()
+{
+    srand(time(NULL)); // use current time as seed for random generator
+    init_pics();
+
+    pthread_t tid;
+    pthread_create(&tid, NULL, thread_func, NULL);
+
+    OPER op = OPER_INVALID;
+    int keyin = '\0';
+    while (true)
+    {
+        keyin = nonblocking_input();
+        op = get_user_op(keyin);
+
+        if (op != OPER_INVALID)
+        {
+            operQueue.push(op);
+        }
+        usleep(20000);
+    }
+
+    pthread_join(tid, NULL);
     return 0;
 }
